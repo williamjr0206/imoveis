393484bdf610c8209907223f03a007af572eb2ab
williamjr0206 <williamjr@milbr.net>
2026-05-18 13:56:59 -0300

    Retirado dados sensíveis.

--- config/database.prod.php
+++ config/database.prod.php
@@ -2,7 +2,7 @@
 
 return [
     'host' => 'localhost',
-    'db'   => 'szjw_fazevedo',
-    'user' => 'szjw_wia',
-    'pass' => 'Wia685618&zenilda'
+    'db'   => '',
+    'user' => '',
+    'pass' => ''
 ];
